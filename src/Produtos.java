5678214bdbf4f8a5776a99861603bb461a671a88
predo505 <pedroesquecido1@gmail.com>
2025-04-18 03:08:17 -0300

    Update Produtos.java

--- src/Produtos.java
+++ src/Produtos.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Produtos {
+
+    String nome;
+    double preco;
+    int quantidade;
+
+    public void exibirDetalhes() {
+        System.out.println("Detalhes do produto");
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: " + preco);
+    }
+    public void calcularTotal() {
+        System.out.println("Quantidade total em estoque: " + quantidade);
+    }
 }

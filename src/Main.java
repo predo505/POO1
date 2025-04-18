c8ab94235dfc5323921b37987b0481f570d8bf64
predo505 <pedroesquecido1@gmail.com>
2025-04-18 03:07:46 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Produtos produto1 = new Produtos();
+        produto1.nome = "Teclado";
+        produto1.preco = 29.99;
+        produto1.quantidade = 10;
+
+        Produtos produto2 = new Produtos();
+        produto2.nome = "Mouse";
+        produto2.preco = 19.99;
+        produto2.quantidade = 12;
+
+        produto1.exibirDetalhes();
+        produto1.calcularTotal();
+        System.out.println(" ");
+        produto2.exibirDetalhes();
+        produto2.calcularTotal();
     }
 }
